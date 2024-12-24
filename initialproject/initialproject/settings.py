ec572f46832b211c6320685171a0e7edc69f94e4
Rojan7 <rojanadhikari02@gmail.com>
2024-12-24 12:31:10 +0000

    admin pannel

--- initialproject/initialproject/settings.py
+++ initialproject/initialproject/settings.py
@@ -41,12 +41,15 @@ INSTALLED_APPS = [
     'all', 
     'tailwind',
     'theme',
+    'django_browser_reload', 
 
 ]
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
-    '127.0.0.1',
+    '127.0.0.1'
 ]
+NPM_BIN_PATH = '/usr/bin/npm' 
+
 
 
 MIDDLEWARE = [
@@ -57,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'initialproject.urls'
--- initialproject/initialproject/urls.py
+++ initialproject/initialproject/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('about/', views.about,name='about'),
     path('contact/', views.contact,name='contact'),
     path('all/', include('all.urls')), 
+
+    
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
