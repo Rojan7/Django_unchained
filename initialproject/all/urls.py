e7bb67b3488af249540a860e9d085d002f6b5c27
Rojan7 <rojanadhikari02@gmail.com>
2024-12-26 22:03:44 +0000

    Final comitt BC day

--- initialproject/all/urls.py
+++ initialproject/all/urls.py
@@ -1,10 +1,11 @@
 
 
 from django.urls import path
-from  all import views
+from  all import views  
 
 urlpatterns = [
     path('', views.all_apps,name='all_app'),
+    path('<int:all_id>', views.all_detail,name='all_detail'),
     
 ]
   
--- initialproject/all/views.py
+++ initialproject/all/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Chaivariety
+from django.shortcuts import get_object_or_404
 
 
 
 # Create your views here.
 def all_apps(request):
     chais = Chaivariety.objects.all()
-    return render(request,'all/all_new_app.html',{'chais':chais})    
+    return render(request,'all/all_new_app.html',{'chais':chais})   
+def all_detail(request,all_id):
+    chai = get_object_or_404(Chaivariety,pk=all_id)
+    return render(request,'all/all_detail.html',{'chai':chai}) 
+ 
