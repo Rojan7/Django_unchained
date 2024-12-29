8f157bc4dd95b14f605bc6d761ebf282648792b2
Rojan7 <rojanadhikari02@gmail.com>
2024-12-29 15:37:13 +0000

    second last comitt

--- initialproject/all/admin.py
+++ initialproject/all/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Chaivariety
+from .models import Chaivariety,Chai_Review,Store,Chai_certificate
+
+class ChaiReviewInline(admin.TabularInline):
+    model = Chai_Review
+    extra = 2
+
+class ChaivarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [ChaiReviewInline]
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_variety',)
+class Chai_certificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'issued_date', 'validity_date')
+
 
 # Register your models here.
-admin.site.register(Chaivariety)
+admin.site.register(Chaivariety,ChaivarietyAdmin)
+admin.site.register(Store,StoreAdmin)
+admin.site.register(Chai_certificate,Chai_certificateAdmin)
+
--- initialproject/all/models.py
+++ initialproject/all/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Chaivariety(models.Model):
     CHAI_TYPE_CHOICE =[
@@ -16,10 +17,30 @@ class Chaivariety(models.Model):
     
     def __str__(self):
         return self.name
-     
+class Chai_Review (models.Model):
+    chai = models.ForeignKey(Chaivariety, on_delete=models.CASCADE,related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.TextField()
+    comment = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f"{self.user.username} review for {self.chai.name}"
+#many to many models
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    chai_variety = models.ManyToManyField(Chaivariety,related_name='stores')
 
 
-
-
-
-# Create your models here.
+    
+    def __str__(self):
+        return self.name
+    
+#one to one
+class Chai_certificate(models.Model):
+    chai = models.OneToOneField(Chaivariety, on_delete=models.CASCADE,related_name='certificate')
+    issued_date = models.DateField(default=timezone.now)
+    validity_date = models.DateField(default=timezone.now)
+    def __str__(self):
+        return f"{self.chai.name}"
